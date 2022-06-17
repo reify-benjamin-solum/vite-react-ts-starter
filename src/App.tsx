afaad26dc298b3e94289dc8b143eb3a8ecc7a2ce
Benjamin Solum <benjamin.solum@reifyhealth.com>
2022-06-17 14:23:47 -0500

    Add linaria

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Header } from 'components/Header';
 import { Button } from 'components/Button';
 import { ReactComponent as Logo } from 'assets/favicon.svg';
+import { cx } from '@linaria/core';
+import { css } from '@linaria/atomic';
+
+const TEST = css`
+  text-transform: uppercase;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <div className={cx('App', TEST)}>
       <Header title="hola" />
       <Logo height={100} width={100} />
       <Button onClick={() => alert('hola')}>Heyo</Button>
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import linaria from '@linaria/rollup';
 
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
   envDir: './env',
-  plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  plugins: [react(), tsconfigPaths(), svgrPlugin(), linaria()],
   server: {
     port: 4000,
   },
