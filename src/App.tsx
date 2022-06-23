221917a8eca5ceddee8cb79674a0e63968fd6bac
Benjamin Solum <benjamin.solum@reifyhealth.com>
2022-06-23 08:28:57 -0500

    cu

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
-import { Header } from 'components/Header';
-import { Button } from 'components/Button';
-import { ReactComponent as Logo } from 'assets/favicon.svg';
 import { cx } from '@linaria/core';
-import { css } from '@linaria/atomic';
+import { Header } from 'components/Header';
+import { Table } from 'components/Table';
+import { ReactComponent as Logo } from 'assets/favicon.svg';
 
-const TEST = css`
-  text-transform: uppercase;
-`;
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+];
+
+const columns = [
+  {
+    label: 'Name',
+    key: 'name',
+  },
+  {
+    label: 'Age',
+    key: 'age',
+    sort: (a, b) => a.age - b.age,
+  },
+  {
+    label: 'Address',
+    key: 'address',
+  },
+];
 
 function App() {
   return (
-    <div className={cx('App', TEST)}>
-      <Header title="hola" />
+    <div className={cx('App')}>
+      <Header title="hey" />
       <Logo height={100} width={100} />
-      <Button onClick={() => alert('hola')}>Heyo</Button>
+      <hr />
+      <Table columns={columns} dataSource={dataSource} />
     </div>
   );
 }
